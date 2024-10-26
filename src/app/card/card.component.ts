9aa5322540e77a89f4ee0adbfd35912670a809ed
alanPalavecino <palavecinoalan2003@gmail.com>
2024-10-26 15:58:00 -0300

    ya la tarjeta funsiona hardcodeada

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+
+export class CardComponent  {
+  isHeartActive = false;
+
+  
+
+  toggleHeart(): void{
+    this.isHeartActive = !this.isHeartActive;
+  }
+
 
 }
