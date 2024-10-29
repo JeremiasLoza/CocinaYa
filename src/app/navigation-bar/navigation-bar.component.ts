8ec84d08eb13ea665c93a567aa82b605d65135a7
Tadeoborneo <tadeoborneo@outlook.com>
2024-10-29 14:28:27 -0300

    Creacion de servicio e interfaz de categorias y agregado de HttpClient al provider de AppModule

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
-export class NavigationBarComponent {
-  categoriesName: String[] = [];
+export class NavigationBarComponent implements OnInit {
   isLogged = false;
   
+  categoriesName: String[] = [];
+  constructor(private categoryService : CategoryService){}
 
+  ngOnInit(){
+    
+  }
 
 
 }
