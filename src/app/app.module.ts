88c79b8ccff7d72ba09ac855b04bc6da640ef26f
Tadeoborneo <tadeoborneo@outlook.com>
2024-10-29 10:16:09 -0300

    Agregado de layout para usuarios y creacion de componente home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 @NgModule({
   declarations: [
     NavigationBarComponent,
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class NavigationBarComponent {
   categoriesName: String[] = [];
-
+  isLogged = false;
   
 
 
