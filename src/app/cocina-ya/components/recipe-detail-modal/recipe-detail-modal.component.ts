3966ff984b4200ac16a7aa535fd8522d9b94e667
Tadeoborneo <tadeoborneo@outlook.com>
2024-11-04 20:25:30 -0300

    Agregado de informacion de cada receta al modal

--- src/app/cocina-ya/components/recipe-detail-modal/recipe-detail-modal.component.ts
+++ src/app/cocina-ya/components/recipe-detail-modal/recipe-detail-modal.component.ts
@@ -1,20 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../models/recipe';
+import { RecipeListService } from '../../services/recipe-list.service';
 
 @Component({
   selector: 'app-recipe-detail-modal',
   templateUrl: './recipe-detail-modal.component.html',
   styleUrls: ['./recipe-detail-modal.component.css']
 })
-export class RecipeDetailModalComponent {
+export class RecipeDetailModalComponent implements OnInit{
   
+  constructor(public recipeListService : RecipeListService){}
   @Input() recipe!: Recipe; 
   @Input() index!: number | null; 
   @Input() recipes!: Recipe[]; 
   @Output() close = new EventEmitter<void>(); 
+  recipeIngredients: string[] = []
+  
+  ngOnInit(): void {
+      this.recipeIngredients = this.recipeListService.getRecipeIngredients(this.recipe);
+  }
+
+  getMeasure(recipe: Recipe, index: number): string {
+    const measureKey = `strMeasure${index}`;
+    return recipe[measureKey as keyof Recipe] || ''; 
+  }
 
   closeModal(): void {
     this.close.emit(); 
   }
-
 }
